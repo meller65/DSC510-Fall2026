1e30db98ddbbd8f3c1bd63d2b2050e2754bcc118
hwardynski <hwardynski@my365.bellevue.edu>
2026-09-09 09:51:58 -0500

    Add files via upload

---
+++ WARDYNSKI_DSC510/Wardynski.Week1Assignment.py
@@ -0,0 +1,6 @@
+# DSC 510
+# Author: Holly Wardynski
+# Title: Week 1 Programming Assignment
+# Purpose: Print Hello World
+#------------------------------------------------------------------------------------
+print("Hello World")
