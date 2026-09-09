88d0585538586cd6a6e1830233f6271cf162e5fb
DBowen402 <dbowen@my365.bellevue.edu>
2026-09-08 23:13:04 -0500

    Add Week 1 PyCharm assignment

---
+++ Bowen-DSC510/bowen-dsc510-week1.py
@@ -0,0 +1,13 @@
+#============================================
+#DSC 510
+#Week 1
+#Programming Assignment Week 1
+#
+#Title: Week 1 Programming Assignment
+#Author Dustin Bowen
+#Date: 9/8/2026
+#Modified By: N/A
+#Description: Shows a basic Hello World program using Python and PyCharm
+#============================================
+
+print("Hello World")
