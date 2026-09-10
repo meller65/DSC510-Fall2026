abbbf2d6d4e422b676da16ab92e17a22fd9c731a
jenfl <jenbecker@gmail.com>
2026-09-10 09:26:15 -0400

    Initial commit of Hello World

---
+++ Becker_DSC510/Becker_Week1.py
@@ -0,0 +1,11 @@
+# ============================================
+# Title: Assignment 1.1
+# Author: Jennifer Becker
+# Date: 05 September 2026
+# Modified By: n/a
+# Description: A simple Hello, World program to demonstrate
+# that Python and PyCharm are working correctly.
+# ===========================================
+
+if __name__ == '__main__':
+    print("Hello, World")
