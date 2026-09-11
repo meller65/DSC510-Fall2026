c9b721e9fa86ba9afd5acaa062e4f5a0da52d9d4
Kevin Bratetic <bratetic@gmail.com>
2026-09-11 16:20:33 -0500

    Add Week 1 Hello World

---
+++ Bratetic_DSC510/bratetic_week1.py
@@ -0,0 +1,11 @@
+# -----------------------------
+# Title: DSC 510 Week 1
+# Author: Kevin Bratetic
+# Date: 11 September 2026
+# Modified By:
+# Description: Prints a Hello World message to confirm Python and PyCharm are set up correctly.
+# Sources: No third-party code was reused.
+# -----------------------------
+
+# Display a greeting in the console
+print("Hello World")
