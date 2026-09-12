a795951ffe2e23d7879af9396426967bbfd512b4
h0tdawg <sschlosser@my365.bellevue.edu>
2026-09-12 10:33:09 -0500

    pycharm assignment 1.1

---
+++ schlosser_DSC510/hello_world.py
@@ -0,0 +1,10 @@
+'''
+============================================
+; Title: Assignment 1.1
+; Author: Sean Schlosser
+; Date: 12 Sept 2026
+; Modified By: <Sean Schlosser>
+; Description: hello world PyCharm Assignment
+;===========================================
+'''
+print("Hello World")
