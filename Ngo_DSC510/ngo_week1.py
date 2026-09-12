b7b6db17e0e98d0d761124a6079fb03c2c824afd
Colby Ngo <your.email@example.com>
2026-09-11 20:53:35 -0700

    Add Hello World program

---
+++ Ngo_DSC510/ngo_week1.py
@@ -0,0 +1,8 @@
+# ============================================
+# Title: Assignment 1.1
+# Author: Colby Ngo
+# Date: 11 September 2026
+# Modified By: N/A
+# Description: This program prints a "Hello World" message using Python & PyCharm
+# ===========================================
+print('Hello World')
